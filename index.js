ef86096e143632ddbd43187ccb4aae6956384370
Joonas Pilli <joonas@linear.fi>
2020-03-16 18:44:48 +0200

    Allow adding icon

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 window.addEventListener("load", () => {
   const scroll = document.querySelector(".js-scroll");
   const output = document.querySelector(".js-output");
+  const questIcon = document.querySelector(".js-quest-icon");
+  const addImgInput = document.querySelector(".js-add-img");
+  const removeImgButton = document.querySelector(".js-remove-img");
+
+  const toBase64 = file =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+    });
 
   const print = () => {
-    domtoimage.toPng(scroll).then(img => {
-      output.src = img;
-    });
+    domtoimage
+      .toPng(scroll)
+      .then(img => {
+        output.src = img;
+      })
+      .catch(console.error);
   };
 
   print();
   scroll.addEventListener("keyup", print);
+
+  addImgInput.addEventListener("change", function() {
+    const files = this.files;
+    if (files && files[0]) {
+      toBase64(files[0]).then(img => {
+        questIcon.src = img;
+        questIcon.classList.add("scroll__quest-icon--visible");
+        print();
+      });
+    }
+  });
+
+  removeImgButton.addEventListener("click", () => {
+    questIcon.src = "./scroll.png";
+    questIcon.classList.remove("scroll__quest-icon--visible");
+    print();
+  });
 });
